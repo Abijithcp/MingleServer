d1fb8c189079e7853355132cc99db82e220b9c22
abijith <abijithcp66@gmail.com>
2023-07-27 18:12:38 +0530

    first commit

---
+++ backend/asgi.py
@@ -0,0 +1,64 @@
+"""
+ASGI config for backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+
+
+
+
+
+
+
+# backend/asgi.py
+# import os
+
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from django.core.asgi import get_asgi_application
+# from channels.security.websocket import AllowedHostsOriginValidator
+# from channels.auth import AuthMiddlewareStack
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+# django_asgi_app = get_asgi_application()
+# import chat.routing
+# application = ProtocolTypeRouter(
+#     {
+#         "http": django_asgi_app,
+#         "websocket": AllowedHostsOriginValidator(
+#             AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+#         ),
+#     }
+# )
+
+
+
+import os
+import django
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+django_asgi_app =  get_asgi_application()
+
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from chat.consumers import ChatConsumer
+from django.urls import path
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+
+    # WebSocket chat handler
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter([
+               path('wss/chat/<str:room_name>/<int:user_id>/', ChatConsumer.as_asgi()),
+            ])
+        )
+    ),
+})
